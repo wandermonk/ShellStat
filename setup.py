0b43535c92372e52500ca4b8619829d1f740df03
PHANI KUMAR YADAVILLI <phanikumaryadavilli@gmail.com>
2023-08-13 11:07:32 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -16,19 +16,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/wandermonk/shellstat",
     packages=find_packages(),
-    install_requires=[
-        'Flask'>=2.3.2,
-        'matplotlib'>=3.7.2,
-        'numpy'>=1.25.2,
-        'pandas'>=2.0.3,
-        'scikit-learn'>=1.3.0,
-        'scipy'>=1.11.1,
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    install_requires=requirements,
     python_requires=">=3.6",
     entry_points={
         'console_scripts': [
